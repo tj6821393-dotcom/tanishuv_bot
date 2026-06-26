aab45e73d7603a57cf97164cce90dbb0a27d9a1d
tom jerry <tj6821393@gmail.com>
2026-06-27 02:15:24 +0500

    Update main_menu.py

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -1,1 +1,10 @@
-.
+from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+
+def main_menu():
+    keyboard = [
+        ["🗺️ Xarita", "🔍 Qidiruv"],
+        ["🔔 Bildirishnomalar", "💌 Xabarlar"],
+        ["🛍️ Do'kon", "👤 Profil"],
+        ["📊 Statistika", "⚙️ Sozlamalar"]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
