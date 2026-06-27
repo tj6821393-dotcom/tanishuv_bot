8268b78e3af764b97d3c64ece323f76f39467781
tom jerry <tj6821393@gmail.com>
2026-06-27 08:09:37 +0500

    Update auth.py

--- bot/middlewares/auth.py
+++ bot/middlewares/auth.py
@@ -1,1 +1,11 @@
-.
+from telegram import Update
+from telegram.ext import ContextTypes
+from bot.database.queries import get_user
+
+async def check_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.effective_user:
+        return
+    user = await get_user(update.effective_user.id)
+    if user and user['is_blocked']:
+        await update.message.reply_text("🚫 Hisobingiz bloklangan.")
+        return
