039a9bd6621becc2f65265604101f6edf2b29ebb
tom jerry <tj6821393@gmail.com>
2026-06-27 08:11:26 +0500

    Update location.py

--- bot/utils/location.py
+++ bot/utils/location.py
@@ -1,1 +1,9 @@
-.
+from bot.utils.matching import calculate_distance
+
+def get_distance_text(lat1, lon1, lat2, lon2) -> str:
+    if not all([lat1, lon1, lat2, lon2]):
+        return "Noma'lum"
+    dist = calculate_distance(lat1, lon1, lat2, lon2)
+    if dist < 1:
+        return f"{int(dist * 1000)} m"
+    return f"{dist:.1f} km"
